8e6e359eb6e3403000208b9dae21aa61498227fd
Sergio Moreno <samgdev91@gmail.com>
2024-06-21 13:02:43 +0200

    21-06

--- consultas.php
+++ consultas.php
@@ -59,12 +59,24 @@ require_once 'conexion.php';
 	}
 
 
-	function getPermisos() {
-
+	function getPermisos():int {
+	try{
+		$con = new Conexion();
+		$sql = "SELECT management FROM setup";
+		$stmt = $con->crearConexion()->prepare($sql);
+		$stmt->execute();
+		$value = $stmt->fetch();
+		$con->cerrarConexion();
+		return $value;
+	}catch(Exception $e){
+		print_r($e->getMessage());
+		return 0;
 	}
+}
 
 
 	function cambiarPermisos():bool {
+		//ESTA TAMBIEN PUEDE HACERSE CON getPermisos
 	try{
 		$con = new Conexion();
 		$sql = "UPDATE user
@@ -85,43 +97,127 @@ require_once 'conexion.php';
 	}
 
 
-	function getCategorias() {
+	function getCategorias():array {
+		try{
+			$con = new Conexion();
+			$sql = "SELECT * FROM category";
+			$stmt=$con->crearConexion()->prepare($sql);
+			$stmt->execute();
+			$rows = $stmt->fetchAll();
+			return $rows;
+		}catch(Exception $e){
+			echo $e->getMessage();
+			return[];
+		}
 	
 	}
 
 
-	function getListaUsuarios() {
+	function getListaUsuarios():array {
 	try{
 		$con = new Conexion();
 		$sql = "SELECT full_name, email, enabled FROM user";
 		$stmt =$con->crearConexion()->prepare($sql);
 		$stmt->execute();
 		$rows = $stmt->fetchAll();
+		$con->cerrarConexion();
 		return $rows;
 	}catch(Exception $e){
 		print_r($e->getMessage());
+		return[];
 	}
      
 	}
 
 
-	function getProducto($ID) {
-
+	function getProducto($ID):array {
+      try{
+		$sql = "SELECT * FROM product WHERE id = ?";
+		$con = new Conexion();
+		$stmt=$con->crearConexion()->prepare($sql);
+		$stmt->bindParam(1, $ID);
+		$stmt->execute();
+		$row = $stmt->fetch();
+		$con->cerrarConexion();
+	    if ($row) {
+            return $row; 
+        }
+        return [];
+	  }catch(Exception $e){
+		print_r($e->getMessage());
+		return[];
+	}
 		
 	}
 
 
-	function getProductos($orden) {
-
+	function getProductos($orden):array {
+		try{
+			$con = new Conexion();
+			$sql ="SELECT p.id, p.name, p.cost, p.price, c.name
+			FROM category c
+			INNER JOIN product p
+			ON c.id = p.category_id
+			ORDER BY ? ";
+			$stmt=$con->crearConexion()->prepare($sql);
+			$stmt->bindParam(1, $orden);
+			$stmt->execute();
+			$rows = $stmt->fetchAll();
+			$con->cerrarConexion();
+			if($rows){
+			   return $rows;
+			}return [];
+		}catch(Exception $e){
+			print_r($e->getMessage());
+			return[];
+		}
 		}
 
 
-	function anadirProducto($nombre, $coste, $precio, $categoria) {
-
+	function anadirProducto($nombre, $coste, $precio, $categoria):bool {
+       try{
+          $nombre = htmlspecialchars($nombre);
+		  $coste = doubleval($coste)?$coste:null;
+		  $precio = doubleval($precio)?$precio:null;
+          $categoria = intval($categoria)?$categoria:null;
+		  if($categoria > 0 && $categoria <5){
+			$con = new Conexion();
+			$sql ="INSERT INTO products(name, cost, price, category_id) values(?,?,?;,?)";
+			$stmt=$con->crearConexion()->prepare($sql);
+			$stmt->bindParam(1, $nombre);
+			$stmt->bindParam(2, $coste);
+			$stmt->bindParam(3, $price);
+			$stmt->bindParam(4, $categoria);
+			$stmt->execute();
+			$con->cerrarConexion();
+			return true;
+		  }return false;
+	   }catch(Exception $e){
+			print_r($e->getMessage());
+			return false;
+		}
 	}
 
 
-	function borrarProducto($id) {
+	function borrarProducto($id):bool {
+	try{
+		
+		$con = new Conexion();
+		$sql = "DELETE FROM products WHERE id = ?";
+		if(filter_var($id, FILTER_VALIDATE_INT)){
+			$existe = count(getProducto($id))>0;
+			if($existe){
+				$stmt = $con->crearConexion()->prepare($sql);
+				$stmt->bindParam(1, $id, PDO::PARAM_INT);
+				$stmt->execute();
+				return true;
+			}echo"producto no existe en la base de datos";
+			return false;
+		}return false;
+	}catch(Exception $e){
+		print_r($e->getMessage());
+		return false;
+	}
 	
 	}
 
